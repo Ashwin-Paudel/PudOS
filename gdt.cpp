def11135f8d473c6b29a63b8b94de836760a2b01
Ashwin-Paudel <angrygamersgaming@gmail.com>
2021-02-15 19:42:00 -0500

    new files added

---
+++ gdt.cpp
@@ -0,0 +1,23 @@
+
+#include "gdt.h"
+
+GlobalDescriptorTable::GlobalDescriptorTable(): nullSegmentSelector(0, 0, 0), unusedSegmentSelector(0, 0, 0), codeSegmentSelector(0, 64*1024*1024, 0x9A), dataSegmentSelector(0, 64*1024*1024, 0x92), {
+
+	uint32_t i[2];
+	i[0] = (uint32_t)this;
+	i[1] = sizeof(GlobalDescriptorTable) << 16;
+
+	// We need to run a few assemly code
+	asm volatile("lgdt (%0)"::"p" (((uint8_t *) i)+2));
+}
+
+GlobalDescriptorTable::~GlobalDescriptorTable() {
+
+}
+
+uint16_t GlobalDescriptorTable::DataSegmentSelector() {
+	return (uint8_t*)&dataSegmentSelector - (uint8_t*)this;
+}
+uint16_t GlobalDescriptorTable::CodeSegmentSelector() {
+	return (uint8_t*)&codeSegmentSelector - (uint8_t*)this;
+}
