92b5f3a76fd4cb2638abf86a4cfb94539e3cfbc6
Ashwin-Paudel <angrygamersgaming@gmail.com>
2021-02-16 07:19:01 -0500

    Adds a \n when you print a stirng

--- kernel.cpp
+++ kernel.cpp
@@ -4,14 +4,35 @@
 #include "types.h"
 #include "gdt.h"
 
-
-// This is a very basic printf
-// This will show text in the terminal in the OS
 void printf(char *str) {
     static uint16_t *VideoMemory = (uint16_t *) 0xb8000;
 
+    static uint8_t x = 0, y = 0;
+
     for (int i = 0; str[i] != '\0'; ++i) {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        switch (str[i]) {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if (x >= 80) {
+            x = 0;
+            y++;
+        }
+
+        if (y >= 25) {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x < 80; x++)
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
     }
 }
 
@@ -21,22 +42,17 @@ typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 extern "C" void callConstructors() {
-    for (constructor *i = &start_ctors; i != &end_ctors; i++) {
+    for (constructor *i = &start_ctors; i != &end_ctors; i++)
         (*i)();
-    }
 }
 
-/* 
-|
-|
-|
-|___________________
-*/
+
 
 extern "C" void kernelDidLoad(const void *multiboot_structure, uint32_t /*multiboot_magic*/) {
-    printf("Hello World! --- Ashwin Paudel \n \n");
+    printf("Hello World! --- Ashwin Paudel");
+    printf("Hello World! --- Ashwin Paudel");
 
     GlobalDescriptorTable gdt;
-    // We don't want to stop the kernel so we keep a loop
+
     while (1);
 }
